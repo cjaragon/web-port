6fe47d67dbcb5de2073ec234c83fef61bf2837dc
Chris Aragon <caragon.tpp@gmail.com>
2021-04-05 17:07:51 -0500

    Initiated Project

---
+++ src/routes.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+import Main from './components/main'
+
+export default (
+    <Switch>
+        <Route exact path='/' component={Main} />
+
+    </Switch>
+)
