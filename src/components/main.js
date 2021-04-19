6c66581b0f96586fe69fec9f58c3aa6943d49774
Chris Aragon <caragon.tpp@gmail.com>
2021-04-19 14:31:44 -0500

    ccc

--- src/components/main.js
+++ src/components/main.js
@@ -2,6 +2,10 @@ import React from 'react'
 
 const Main = (props) => {
     <div>
+        <p>
+            <h1> Hey There, I'm Chris </h1>
+            <h3> I am a Web Developer </h3>
+        </p>
 
     </div>
 }
